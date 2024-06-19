354aa7b83bdb63345ef9faa8576a8f1cc2d22bb2
efoxsolution2024 <root@21.36.167.72.host.secureserver.net>
2024-06-19 03:29:20 +0000

    added graph

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -17,65 +17,58 @@ class TestController extends Controller
     $currentMonth = Carbon::now()->month;
 
     $expiredLists = DB::table('businesses')
-                    ->select('expired_date', 'website')
-                    ->whereYear('expired_date', $currentYear)
-                    ->whereMonth('expired_date', $currentMonth)
-                   ->get();
+                        ->select('expired_date', 'website')
+                        ->whereYear('expired_date', $currentYear)
+                        ->whereMonth('expired_date', $currentMonth)
+                        ->get();
 
-  
     $accountDatas = Business::selectRaw('MONTH(created_at) as month, COUNT(*) as count')
-                    ->whereYear('created_at', date('Y'))
-                    ->groupBy('month')
-                    ->orderBy('month')
-                    ->get();
-
+                        ->whereYear('created_at', $currentYear)
+                        ->whereMonth('created_at', '<=', 6)
+                        ->groupBy('month')
+                        ->orderBy('month')
+                        ->get();   
+  
     $expiredDatas = Business::selectRaw('MONTH(expired_date) as month, COUNT(*) as count')
-                    ->whereYear('expired_date', date('Y'))
-                    ->groupBy('month')
-                    ->orderBy('month')
-                    ->get();
-
+                        ->whereYear('expired_date', $currentYear)
+                        ->whereMonth('expired_date', '<=', 6)
+                        ->groupBy('month')
+                        ->orderBy('month')
+                        ->get();
+                        
     $labels = [];
-    $dataNewAccounts = [];
-    $dataExpiredAccounts = [];
+    $dataNewAccounts = array_fill(0, 6, 0); 
+    $dataExpiredAccounts = array_fill(0, 6, 0); 
     $color = ['#42f54e', '#f2072e'];
 
-
-    $dataNewAccounts = array_fill(1, 12, 0);
-    $dataExpiredAccounts = array_fill(1, 12, 0);
-   
     foreach ($accountDatas as $accountData) {
-        $dataNewAccounts[$accountData->month] = $accountData->count;
+        $dataNewAccounts[$accountData->month - 1] = $accountData->count; 
     }
 
     foreach ($expiredDatas as $expiredData) {
-        $dataExpiredAccounts[$expiredData->month] = $expiredData->count;
-    }  
-    
-    // Populate labels array
-    for ($i = 1; $i <= 12; $i++) {
-        $month = date('F', mktime(0, 0, 0, $i, 1));
-        array_push($labels, $month);
+        $dataExpiredAccounts[$expiredData->month - 1] = $expiredData->count;
     }
 
-    // Prepare datasets for chart
-    $datasets = [
-        [
-            'label' => 'New Accounts',
-            'data' => array_values($dataNewAccounts), // Convert associative array to indexed array
-            'backgroundColor' => $color[0],
-        ],
-        [
-            'label' => 'Expired Accounts',
-            'data' => array_values($dataExpiredAccounts), // Convert associative array to indexed array
-            'backgroundColor' => $color[1],
-        ]
+    for ($i = 1; $i <= 6; $i++) {
+        $month = date('F', mktime(0, 0, 0, $i, 1));
+        array_push($labels, $month);
+    }  
+
+    $newAccounts = [
+        'data' => array_values($dataNewAccounts) 
+    ];
+
+    $expiredAccount = [
+        'data' => array_values($dataExpiredAccounts) 
     ];
 
     return view('navigation.index', [
         'labels' => $labels,
-        'datasets' => $datasets,
         'expiredLists' => $expiredLists,
+        'newAccounts' => $newAccounts,
+        'expiredAccount' => $expiredAccount,
+        'firstquarterNew' => array_sum($dataNewAccounts),
+        'firstquarterExpired' => array_sum($dataExpiredAccounts),
     ]);
 }
 
